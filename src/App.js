4fe41ce7f527a65a726f8233ad660e4da2b36447
eugene huang <eugene@goforward.com>
2023-07-28 01:12:11 -0700

    add chessboard with move validation
    debug and massively simplify a chessboardjsx example. convert to
    functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chess } from "chess.js";
+import Chessboard from "chessboardjsx";
 
-function App() {
+export default function App() {
+  const [fen, setFen] = React.useState("start");
+  const [dropSquareStyle, setDropSquareStyle] = React.useState({});
+  const [squareStyles, setSquareStyles] = React.useState({});
+
+  const game = React.useRef(new Chess());
+
+  const onDrop = ({ sourceSquare, targetSquare }) => {
+    try {
+      game.current.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q" // warn: always promote to a queen
+      });
+    } catch (error) {
+      return;
+    }
+
+    setFen(game.current.fen());
+    setSquareStyles(squareStyling(game.current.history({ verbose: true })))
+  };
+
+  const onDragOverSquare = _ => {
+    setDropSquareStyle({ boxShadow: "inset 0 0 1px 4px rgb(255, 255, 0)" })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Chessboard
+      id="board"
+      width={400}
+      position={fen}
+      onDrop={onDrop}
+      boardStyle={{
+        borderRadius: "5px",
+        boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+      }}
+      squareStyles={squareStyles}
+      dropSquareStyle={dropSquareStyle}
+      onDragOverSquare={onDragOverSquare}
+    />
+  )
 }
 
-export default App;
+const squareStyling = (history) => {
+  console.log(history)
+  const sourceSquare = history.length && history[history.length - 1].from;
+  const targetSquare = history.length && history[history.length - 1].to;
+
+  return {
+    ...(history.length && {
+      [sourceSquare]: {
+        backgroundColor: "rgba(255, 255, 0, 0.4)"
+      }
+    }),
+    ...(history.length && {
+      [targetSquare]: {
+        backgroundColor: "rgba(255, 255, 0, 0.4)"
+      }
+    })
+  };
+};
